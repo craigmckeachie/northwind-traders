c2f65ce1480dabc420e393c35e79a71002a7832c
Craig McKeachie <craigdmckeachie@gmail.com>
2025-12-03 13:47:48 -0500

    hard code getAll CustomerDao shorten

--- src/main/java/com/northwind/data/CustomerDao.java
+++ src/main/java/com/northwind/data/CustomerDao.java
@@ -15,23 +15,24 @@ public class CustomerDao {
 
     public List<Customer> getAll(){
         List<Customer> customers = new ArrayList<>();
+
         // Customer 1: Alfreds Futterkiste
-        Customer customer1 = new Customer(
+        customers.add(new Customer(
                 "ALFKI",
                 "Alfreds Futterkiste",
                 "Maria Anders",
                 "Sales Representative",
                 "Obere Str. 57",
                 "Berlin",
-                null, // Region is often null for some Northwind data
+                null,
                 "12209",
                 "Germany",
                 "030-0074321",
                 "030-0076545"
-        );
+        ));
 
         // Customer 2: Ana Trujillo Emparedados y helados
-        Customer customer2 = new Customer(
+        customers.add(new Customer(
                 "ANATR",
                 "Ana Trujillo Emparedados y helados",
                 "Ana Trujillo",
@@ -43,10 +44,10 @@ public class CustomerDao {
                 "Mexico",
                 "(5) 555-4729",
                 "(5) 555-3745"
-        );
+        ));
 
         // Customer 3: Antonio Moreno Taquería
-        Customer customer3 = new Customer(
+        customers.add(new Customer(
                 "ANTON",
                 "Antonio Moreno Taquería",
                 "Antonio Moreno",
@@ -57,11 +58,11 @@ public class CustomerDao {
                 "05023",
                 "Mexico",
                 "(5) 555-3932",
-                null // Fax is often null/missing in some Northwind data
-        );
+                null
+        ));
 
         // Customer 4: Around the Horn
-        Customer customer4 = new Customer(
+        customers.add(new Customer(
                 "AROUT",
                 "Around the Horn",
                 "Thomas Hardy",
@@ -73,10 +74,10 @@ public class CustomerDao {
                 "UK",
                 "(171) 555-7788",
                 "(171) 555-6750"
-        );
+        ));
 
         // Customer 5: Berglunds snabbköp
-        Customer customer5 = new Customer(
+        customers.add(new Customer(
                 "BERGS",
                 "Berglunds snabbköp",
                 "Christina Berglund",
@@ -88,13 +89,7 @@ public class CustomerDao {
                 "Sweden",
                 "0921-12 34 65",
                 "0921-12 34 67"
-        );
-
-        customers.add(customer1);
-        customers.add(customer2);
-        customers.add(customer3);
-        customers.add(customer4);
-        customers.add(customer5);
+        ));
 
 
         return customers;
